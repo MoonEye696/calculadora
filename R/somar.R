c7b555d0d7dbe03c48edf22286e66e3f9b139ce5
lucas <lucascoder14@gmail.com>
2021-11-18 16:18:14 -0300

    First Commit, criada a função somar e subtrair

---
+++ R/somar.R
@@ -0,0 +1,13 @@
+#' função para subtrair
+#' está função serve para subtrair
+#'
+#' @param a um número
+#' @param b outro número
+#'
+#' @example
+#' somar (3, 4)
+#'
+#' @export
+somar <- function(a, b){
+  a + b
+}
---
+++ R/subtrair.R
@@ -0,0 +1,13 @@
+#' função para subtrair
+#' está função serve para subtrair
+#'
+#' @param a um número
+#' @param b outro número
+#'
+#' @example
+#' subtrrair (7, 4)
+#'
+#' @export
+subtrair <- function(a , b){
+  a - b
+}
